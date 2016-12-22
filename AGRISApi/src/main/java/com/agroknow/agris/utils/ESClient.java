94a7ca2a21482027993bab38164263992d98dfe9
mihalispap <mihalispapak@gmail.com>
2016-12-22 18:27:44 +0200

    2nd node

--- AGRISApi/src/main/java/com/agroknow/agris/utils/ESClient.java
+++ AGRISApi/src/main/java/com/agroknow/agris/utils/ESClient.java
@@ -8,20 +8,25 @@ import org.elasticsearch.common.transport.InetSocketTransportAddress;
 
 public class ESClient {
 	
-	//public static String server="localhost";
-	public static String server="83.212.100.195";
-	public static Settings settings = ImmutableSettings.settingsBuilder()
-    	.put("cluster.name", "agroknow").build();
-	public void ESClient()
-	{
+		//public static String server="localhost";
+		public static String server1="83.212.100.195";
+		public static String server2="83.212.101.19";
+		
+		public static Settings settings = ImmutableSettings.settingsBuilder()
+	    	.put("cluster.name", "agroknow").build();
+		public void ESClient()
+		{
 
-		settings = ImmutableSettings.settingsBuilder()
-		        .put("cluster.name", "agroknow").build();
-		client = new TransportClient(settings)
-		        .addTransportAddress(new InetSocketTransportAddress(server, 9300));
-	}
-	
-	public static Client client = new TransportClient(settings)
-	        .addTransportAddress(new InetSocketTransportAddress(server, 9300));
+			settings = ImmutableSettings.settingsBuilder()
+			        .put("cluster.name", "agroknow").build();
+			client = new TransportClient(settings)
+			        .addTransportAddress(new InetSocketTransportAddress(server1, 9300))
+			        .addTransportAddress(new InetSocketTransportAddress(server2, 9300))
+			        ;
+		}
+		
+		public static Client client = new TransportClient(settings)
+		        .addTransportAddress(new InetSocketTransportAddress(server1, 9300))
+		        .addTransportAddress(new InetSocketTransportAddress(server2, 9300));
 	
 }
