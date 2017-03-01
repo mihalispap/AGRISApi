82f7f830250ed286ca87210f456acd950287e1da
mihalispap <mihalispapak@gmail.com>
2017-03-01 12:22:03 +0200

    updt sfth id

--- AGRISApi/src/main/java/com/agroknow/agris/controllers/IDEndpoint.java
+++ AGRISApi/src/main/java/com/agroknow/agris/controllers/IDEndpoint.java
@@ -337,7 +337,175 @@ public class IDEndpoint {
     }
     
 
-	
+	@ApiOperation(value = "Get course by ID", nickname = "find entity values by ID")
+    @RequestMapping(method = RequestMethod.GET, path="/course/{id}",produces="text/plain"
+    /*, produces = {"application/json","application/xml"}*/)
+	@ApiImplicitParams({
+        @ApiImplicitParam(
+        			name = "id", 
+        			value = "course ID", 
+        			required = true, 
+        			dataType = "string", 
+        			paramType = "path", 
+        			defaultValue="8"),
+        @ApiImplicitParam(
+    			name = "format", 
+    			value = "output format", 
+    			required = true, 
+    			dataType = "string", 
+    			paramType = "query",
+    			defaultValue="json"),
+		@ApiImplicitParam(
+    	    	name = "apikey", 
+    	    	value = "apikey", 
+    	    	required = true, 
+    	    	dataType = "string", 
+    	    	paramType = "query", 
+    	    	defaultValue="sfth-dev"),
+		@ApiImplicitParam(
+    			name = "cache", 
+    			value = "use cache", 
+    			required = true, 
+    			dataType = "boolean", 
+    			paramType = "query",
+    			defaultValue="true")
+      })	
+	ResponseEntity<String> runCourse(@PathVariable String id, HttpServletRequest request) {
+		
+		ParseGET parser=new ParseGET();
+		
+		String apikey=parser.parseApiKey(request);
+		
+		AGRISMongoDB mongodb = new AGRISMongoDB();
+		
+		boolean mongo_up=true;
+		
+		String format;
+		//ParseGET parser=new ParseGET();
+		format=parser.parseFormat(request);
+		HttpHeaders response_head=new HttpHeaders();
+		
+		if(format.equals("xml"))
+			response_head.setContentType(new MediaType("application","xml"));
+		else
+			response_head.setContentType(new MediaType("application","json"));
+		
+		try
+		{
+			if(!mongodb.isValidApiKey(apikey))
+			{
+				//return "{\"error\":\"Api validation Error\"}";
+				return new ResponseEntity<String>("{\"error\":\"Api validation Error\"}", response_head, HttpStatus.CREATED);
+				
+			}
+			
+			mongodb.addPoints(apikey, "id", request);
+			
+			try
+			{
+				boolean use_cache=parser.parseCache(request);			
+				
+				if(use_cache)
+				{
+					String cached = mongodb.checkCache(request);
+					if(!cached.isEmpty())
+					{
+						System.out.println("CACHE HIT!");			
+						double end_time=(double)System.currentTimeMillis()/1000;
+						//System.out.println("Took:"+(double)(end_time-init_time));
+						//return cached;
+						
+						return new ResponseEntity<String>(cached, response_head, HttpStatus.CREATED);
+						
+					}
+				}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		catch(Exception e)
+		{
+			mongo_up=false;
+		}
+		
+		
+    	Settings settings = ImmutableSettings.settingsBuilder()
+		        .put("cluster.name", "agroknow").build();
+    	
+    	Client client = ESClient.client;
+    	
+		//System.out.println("Status:"+client.settings().toString());
+		// on shutdown
+		
+		GetResponse response = client.prepareGet("sfth", "course", id)
+		        .execute()
+		        .actionGet();
+		
+		BoolQueryBuilder build_o =QueryBuilders.boolQuery();
+		build_o.must(QueryBuilders.termQuery("course.identifier", id));
+		/*HasParentQueryBuilder qb=QueryBuilders.hasParentQuery("object",
+					QueryBuilders.matchQuery("appid", id));*/
+
+		SearchResponse responseSpecific=client
+				.prepareSearch("sfth")
+				.setQuery(build_o)
+				//.setSize(1)
+				.execute()
+				.actionGet();
+
+		
+		String results="";
+		
+		/*int size=0;
+		try
+		{
+			if(!response.getSourceAsString().isEmpty())
+				size=1;
+		}
+		catch(java.lang.NullPointerException e)
+		{
+
+			return new ResponseEntity<String>("{\"total\":0"
+					+ ",\"results\":[]"
+					+ "}", response_head, HttpStatus.CREATED);
+		}
+		 */
+		
+		int size=0;
+		String specific_source="";
+		
+		for(SearchHit hit : responseSpecific.getHits().getHits())
+		{
+			specific_source=hit.getSourceAsString();
+			size++;
+			break;
+		}
+		
+		
+
+		results+="{\"total\":"+size+",\"results\":[{"
+				+ "\"course\":"+specific_source+"}";
+						//+ "\"detailed\":"+responseSpecific.getSourceAsString()+"";
+		
+		
+		
+		results+="]}";
+		
+		if(format.equals("xml"))
+		{
+			ToXML converter=new ToXML();
+			results=converter.convertToXMLID(results);
+		}
+		
+
+		if(mongo_up)
+			mongodb.cacheResponse(request, results);
+		
+    	//return results;
+    	return new ResponseEntity<String>(results, response_head, HttpStatus.CREATED);
+    }
 
 
 }
